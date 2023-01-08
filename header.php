2e805aebfbb8187447c50ffcbd1c913e75906c77
IkoAlmasDevGame <ikoalmas27@gmail.com>
2023-01-08 14:38:04 +0700

    first commit

---
+++ header.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./bootstraps/css/bootstrapv5221.css" type="text/css" crossorigin>
+    <script src="./bootstraps/js/bootstrap.min.js" type="text/javascript"></script>
+    <link rel="stylesheet" href="./bootstraps/css/text-bootstrap.css" type="text/css" crossorigin>
+    <link rel="stylesheet" href="./bootstraps/css/glyphicon.css" type="text/css" crossorigin>
+    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="./bootstraps/css/font-awesome4.min.css">
+    <link rel="stylesheet" href="./bootstraps/css/font-awesome.min.css">
+    <title>Portofolio</title>
+</head>
+    <body id="background">
+        <div id="layoutAuthentication">
+            <div id="layoutAuthentication_content">
+                <main>
+                    <div class="navbar navbar-default">
+                        <div class="container-fluid">
+                            <div class="navbar-header">
+                                <div class="navbar-brand"><a href="./index.php" class="text-large">Portofolio</a></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-2">
+                        <div class="row">
+                            <div class="col-xs-12" style="top: 9vh;">
+                                <a class="img-thumbnail">
+                                    <img src="./image/Iko_Almas-removebg-preview.png" class="img-responsive">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="row pt-5 mb-5"></div>
+                        <ul class="nav nav-pills nav-stacked">
+                            <li class="text-large"><a href="https://github.com/IkoAlmasDevGame/">
+                                <span class="fa fa-github" style="color: black;">  Github Me</span>
+                            </a></li>
+                            <li class="text-large"><a href="https://wa.me/6289622482180">
+                                <span class="fa fa-phone" style="color: black;">  Whatsapp Number</span>
+                            </a></li>
+                            <li class="text-large"><a>
+                                <span class="fa fa-envelope" style="color: black;"> ikoalmas27@gmail.com</span>
+                            </a></li>
+                            <li class="text-large"><a>
+                                <span class="fa fa-map-marker" style="color: black;">
+                                  street haji ismail no.62, jatinegara, cakung
+                                </span>
+                            </a></li>
+                        </ul>
+                    </div>
+                </main>
+            </div>
+        </div>
+    <div class="col-md-2 mx-auto">
---
+++ index.php
@@ -0,0 +1,139 @@
+<?php 
+include "header.php";
+?>
+
+<div class="col-md-auto col-md-offset-0">
+<h3 style="text-align:start; font-size: 32px; font-weight:normal;">Portofolio</h3>
+    <table class="table table-bordered">
+        <div class="mb-auto">
+            <tr>
+                <th class="input-group">
+                    <span class="input-group-addon text-center text-large">
+                        <label for="nama">Iko Almas Qistan</label>
+                    </span>
+                </th>
+            </tr>
+        </div>
+        <div class="mb-auto">
+            <tr>
+                <th class="input-group">
+                    <span class="input-group-addon text-center text-large">
+                        <label for="">SOFTWARE ENGINEER</label>
+                    </span>                
+                </th>
+            </tr>
+        </div>
+        <div class="mb-auto">
+                <tr>
+                    <th class="input-group">
+                        <span class="input-group-addon text-start text-large">
+                            <label for="">About</label>
+                            <p class="text-large text-start mb-2">
+                            Perkenalkan nama saya Iko almas qistan, saya berkuliah
+                            <p class="text-large text-start mb-2">
+                            di salah satu universitas di jakarta yaitu universitas bina sarana informatika,
+                            </p>
+                            <p class="text-large text-start mb-2">
+                             saya adalah fresh graduate di universitas bina sarana informatika.
+                            </p>                             
+                        </span>                
+                    </th>
+                </tr>
+            </div>
+            <div class="mb-auto">
+                <tr>
+                    <th class="input-group">
+                        <span class="input-group-addon text-start text-large">
+                            <label for="">Experience</label>
+                            <p class="text-large mb-2 table table-borderd">
+                            2019 - 2020
+                                <p>
+                                * Pernah mengerjakan tugas kuliah membuat sebuah aplikasi di android stuido menggunakan flutter, 
+                                <br>
+                                mengembangkan system penjualan di android studio menggunakan code flutter dart.
+                                </p>
+                            </p>
+                            <p class="text-large mb-2 table table-borderd">
+                            2021 - 2022
+                                <p>
+                                * Membuat project website kecil-kecilan menggunakan php (personal home page)
+                                </p>
+                                <p>
+                                * Database yang saya gunakan mysql di phpmyadmin secara coding menggunakan mysql.
+                                </p>
+                                <p>
+                                * saya sedang membuat project kedua yaitu warung dengan sistem pembayaran dan bisa dicetak atau diprint sebagai hasil data pembayaran.
+                                </p>
+                                <p>
+                                * sedang mengembangkan website project menggunakan javascript + bootstrap supaya bisa menggunakan class dan id yang ada di bootstrap.
+                                </p>
+                                <p>
+                                * sedang mencoba mempelajari framework CodeIgnitor dengan basic php awal yang sedeharna.
+                                </p>
+                                <p>
+                                * sedang mencoba mempelajari front-end menggunakan bootstrap dan sedikit belajar javascript sederhana tanpa menggunakan library.
+                                </p>
+                            </p>
+                            <p class="text-large mb-2 table table-bordered">
+                            2023 - now
+                            <p>
+                                * sedang mengembangkan react js sebagai project demo project pertama, untuk mempelajari library react-js pada umumnya.
+                            </p>
+                        </p>                          
+                    </span>                
+                </th>
+            </tr>
+        </div>
+        <div class="mb-auto">
+            <tr>
+                <th class="input-group">
+                <span class="input-group-addon text-start text-large">
+                    <label for="">Education</label>
+                        <p class="text-large mb-2 table table-borderd">
+                            vocational high school
+                            <p class="text-large mb-2">
+                                2016 - 2019
+                                <p class="text-large mb-2">
+                                department Accounting
+                                </p>
+                            </p>
+                        </p>
+                        <p class="text-large mb-2 table table-borderd">
+                            universitas bina sarana informatika
+                            <p class="text-large mb-2">
+                                2019 - 2022
+                                <p class="text-large mb-2">
+                                Teknik Informatika<p>3.44/4 GPA</p>
+                                </p>
+                            </p>
+                        </p>
+                    </span>
+                </th>
+            </tr>
+        </div>
+        <div class="mb-auto">
+                <tr>
+                    <th class="input-group">
+                        <span class="input-group-addon text-start text-large">
+                            <label for="">Skill</label>
+                            <p class="text-large mb-2">
+                            <p>
+                            * bahasa pemograman coding : php, python, c++, javascript, mysql, dart.
+                            </p>
+                            <p>
+                            * mampu mengoperasikan database menggunakan mysql di phpmyadmin.
+                            </p>
+                            <p>
+                            * software untuk membuat pemograman coding yaitu visual studio code.
+                            </p>
+                        </p>                        
+                    </span>                
+                </th>
+            </tr>
+        </div>
+    </table>
+</div>
+
+<?php 
+include "footer.php";
+?>
